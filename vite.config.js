23239b8a2dadd355816c8050d4b3f734a346bfac
GarudaPratama <garuda281209@student.abudzar.sch.id>
2025-09-23 10:55:48 +0700

    update

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/MainPortfolio/',
+  base: '/MainPortfolio/', // sesuai nama repo persis
 })
